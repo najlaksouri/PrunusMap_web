6cf0caf9ecc5ffa33b5fa50cf240ab81dd9d6542
Cantalapiedra <cpcantalapiedra@gmail.com>
2017-03-08 09:58:01 +0100

    maps.js

---
+++ js/maps.js
@@ -0,0 +1,9 @@
+/* # index.js is part of Barleymap web app.
+ * # Copyright (C)  2013-2014  Carlos P Cantalapiedra.
+ * # (terms of use can be found within the distributed LICENSE file).
+*/
+$(document).ready(function(){
+    
+    // the script to show/hide graphical maps is in HtmlWriter
+    // before writing the HTML(SVG) of the maps
+});
